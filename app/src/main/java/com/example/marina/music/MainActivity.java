7e0d763fdf6f73f6a2ae0bb4c8c1458e38c77e24
Marina R <www.marina_9205@mail.ru>
2016-03-26 12:21:35 +0200

    Initial commit

---
+++ app/src/main/java/com/example/marina/music/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.marina.music;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_part);
+
+    }
+}
