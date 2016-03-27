180ba154980c6a664ff9c58710167f35f4a48c0b
Marina R <www.marina_9205@mail.ru>
2016-03-27 18:34:07 +0300

    Third commit

--- app/src/main/java/com/example/marina/music/MainActivity.java
+++ app/src/main/java/com/example/marina/music/MainActivity.java
@@ -12,7 +12,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_timer);
+        setContentView(R.layout.activity_description);
 
     }
 }
